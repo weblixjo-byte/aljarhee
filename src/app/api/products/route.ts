27ff983c846a60731a0f8a6764302750b1eb899a
Mohammad Alseidi <mfaraj981@gmail.com>
2026-08-04 20:48:04 +0300

    Optimize favicons, logo size and add Cache-Control headers to API products to save bandwidth

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -6,7 +6,13 @@ export const dynamic = "force-dynamic";
 export async function GET() {
   try {
     const products = await getProductsList();
-    return NextResponse.json(products);
+    return new NextResponse(JSON.stringify(products), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+        "Cache-Control": "public, max-age=120, s-maxage=3600, stale-while-revalidate=86400",
+      },
+    });
   } catch (err: any) {
     console.error("Error in products API route:", err);
     return NextResponse.json(
